4435166c2e75207ab26c5b5ebdecc5486128d590
koushik.modayurchandramoule <koushik.modayurchandramoule@mavs.uta.edu>
2015-11-05 13:44:03 -0600

    push

---
+++ FaceDetection/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace FaceDetection
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.imgCap = new Emgu.CV.UI.ImageBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.imgCap)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imgCap
+            // 
+            this.imgCap.Location = new System.Drawing.Point(40, 36);
+            this.imgCap.Name = "imgCap";
+            this.imgCap.Size = new System.Drawing.Size(505, 415);
+            this.imgCap.TabIndex = 2;
+            this.imgCap.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(551, 173);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Capture";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(745, 479);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.imgCap);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.imgCap)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Emgu.CV.UI.ImageBox imgCap;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ FaceDetection/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.CV.Util;
+using Emgu.CV.UI;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Cuda;
+
+namespace FaceDetection
+{
+    public partial class Form1 : Form
+    {
+        private Capture cap;
+        private CascadeClassifier classify;
+        private CascadeClassifier classify1;
+        private Boolean progress;
+
+
+
+
+        public Form1()
+        {
+
+            InitializeComponent();
+        }
+
+        private void Process(object sender, EventArgs arg)
+        {
+            classify = new CascadeClassifier("haarcascade_frontalface_default.xml");
+            classify1 = new CascadeClassifier("haarcascade_eye.xml");
+
+            Image<Bgr, Byte> img = cap.QueryFrame().ToImage<Bgr, Byte>();
+            if (img != null)
+            {
+                Image<Gray,byte> grayscaleframe = img.Convert<Gray, byte>();
+                var faces = classify.DetectMultiScale(grayscaleframe, 1.1,3,Size.Empty,Size.Empty);
+                var eyes = classify1.DetectMultiScale(grayscaleframe, 1.1, 10,Size.Empty,Size.Empty);
+
+                foreach(var face in faces)
+                {
+                    
+                   img.Draw(face, new Bgr(Color.Blue));
+                }
+
+               foreach(var eye in eyes)
+                {
+                    img.Draw(eye, new Bgr(Color.Red), 2);
+                }
+            }
+
+            imgCap.Image = img;
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (cap == null)
+            {
+                try
+                {
+                    cap = new Capture();
+                }
+                catch (NullReferenceException excpt)
+                {
+                    MessageBox.Show(excpt.Message);
+                }
+            }
+            if (cap != null)
+            {
+                if (progress)
+                {  //if camera is getting frames then stop the capture and set button Text
+                    // "Start" for resuming capture
+                    button1.Text = "Start!"; //
+                    Application.Idle -= Process;
+                }
+                else
+                {
+                    //if camera is NOT getting frames then start the capture and set button
+                    // Text to "Stop" for pausing capture
+                    button1.Text = "Stop";
+                    Application.Idle += Process;
+                }
+
+                progress = !progress;
+            }
+        }
+        private void ReleaseData()
+        {
+            if (cap != null)
+                cap.Dispose();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+                    }
+    }
+}
